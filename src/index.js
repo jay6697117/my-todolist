bf3e3e6779e469a5eb44a1e70f8ad98d0fbf01f4
steve zhang <871506263@qq.com>
2021-06-20 23:54:56 +0800

    提交代码于: 2021-06-20 23:54:56

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
 
 class App extends React.Component {
+  // JSX
   render() {
-    return <h1>Hello, World</h1>;
+    const handleClick = () => {
+      alert('lalala');
+    };
+    const element1 = (
+      <li className='test' onClick={handleClick}>
+        Hello, World
+      </li>
+    );
+    const element2 = (
+      <div>
+        <ul>{element1}</ul>
+      </div>
+    );
+    return element2;
   }
 }
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// const element = <h1>Hello, World</h1>
+// const element = React.createElement('h1', null, 'Hello, World');
+// ReactDOM.render(element, document.getElementById('root'));

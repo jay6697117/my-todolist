d6299d1f7819200142b4ac40730536ad6a2d1b8e
zhangjinhui <871506263@qq.com>
2021-06-21 15:09:18 +0800

    提交代码于: 2021-06-21 15:09:18

--- src/index.js
+++ src/index.js
@@ -2,35 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-const TagInput = ({ tags }) => {
-  const [tagData, setTagData] = React.useState(tags);
-  const removeTagData = indexToRemove => {
-    setTagData([...tagData.filter((_, index) => index !== indexToRemove)]);
-  };
-  const addTagData = event => {
-    if (event.target.value !== '') {
-      setTagData([...tagData, event.target.value]);
-      event.target.value = '';
-    }
-  };
-  return (
-    <div className='tag-input'>
-      <ul className='tags'>
-        {tagData.map((tag, index) => (
-          <li key={index} className='tag'>
-            <span className='tag-title'>{tag}</span>
-            <span className='tag-close-icon' onClick={() => removeTagData(index)}>
-              x
-            </span>
-          </li>
-        ))}
+class Todo extends React.Component {
+  // JSX
+  render() {
+    console.log(`class Todo this.props`, this.props);
+    return <li>{this.props.content}</li>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    const todoList = ['图雀', '图雀写作工具', '图雀社区', '图雀文档'];
+    return (
+      <ul>
+        <Todo content={todoList[0]} />
+        <Todo content={todoList[1]} />
+        <Todo content={todoList[2]} />
+        <Todo content={todoList[3]} />
       </ul>
-      <input
-        type='text'
-        onKeyUp={event => (event.key === 'Enter' ? addTagData(event) : null)}
-        placeholder='Press enter to add a tag'
-      />
-    </div>
-  );
-};
-ReactDOM.render(<TagInput tags={['Nodejs', 'MongoDB']} />, document.getElementById('root'));
+    );
+  }
+}
+
+// function Todo(props) {
+//   console.log(`function Todo props: `, props);
+//   return <li>{props.content}</li>;
+// }
+
+// function App(props) {
+//   const todoList = ['图雀', '图雀写作工具', '图雀社区', '图雀文档'];
+//   return (
+//     <ul>
+//       <Todo content={todoList[0]} />
+//       <Todo content={todoList[1]} />
+//       <Todo content={todoList[2]} />
+//       <Todo content={todoList[3]} />
+//     </ul>
+//   );
+// }
+
+ReactDOM.render(<App />, document.getElementById('root'));

94ae77f4e042a4be943efe5917e0da02f3d77e2e
zhangjinhui <871506263@qq.com>
2021-06-24 20:39:40 +0800

    提交代码于: 2021-06-24 20:39:40

--- src/index.js
+++ src/index.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './assets/style.css';
+
+const todoList = ['图雀', '图雀写作工具', '图雀社区', '图雀文档'];
+const nowTodo = ['哈哈哈', '啦啦啦'];
 
 class Todo extends React.Component {
   // JSX
   render() {
-    console.log(`class Todo this.props`, this.props);
+    console.log(`class Todo render this.props`, this.props);
     return <li>Hello, {this.props.content}</li>;
   }
 }
 
 class App extends React.Component {
+  // React 约定每个继承自 React.Component 的组件在定义 constructor 方法时，要在方法内首行加入 super(props)
+  constructor(props) {
+    super(props);
+
+    //设置初始值
+    this.state = {
+      todoList: [],
+      nowTodo: []
+    };
+  }
+  componentDidMount() {
+    new Promise(resolve => {
+      this.timer1 = setTimeout(resolve, 2000);
+    }).then(() => {
+      this.setState({ todoList, nowTodo });
+    });
+  }
+
+  componentWillUnmount() {
+    console.log(`this.timer1`, this.timer1);
+    clearTimeout(this.timer1);
+  }
   render() {
-    console.log(`class App this.props`, this.props);
-    const todoList = ['图雀', '图雀写作工具', '图雀社区', '图雀文档'];
+    console.log('--------------------------------');
+    console.log(`class App render this.state`, this.state);
     return (
       <ul>
-        <Todo content={todoList[0]} />
-        <Todo content={todoList[1]} />
-        <Todo content={todoList[2]} />
-        <Todo content={todoList[3]} />
+        <Todo content={this.state.todoList[0]} />
+        <Todo content={this.state.todoList[1]} />
+        <Todo content={this.state.todoList[2]} />
+        <Todo content={this.state.todoList[3]} />
+        <Todo content={this.state.nowTodo[0]} />
+        <Todo content={this.state.nowTodo[1]} />
       </ul>
     );
   }
